499e71dbb3b0fe4f9c7b00d1d86dcf0bb63e6a41
Kem <tuetran25112002@gmail.com>
2023-11-15 18:21:38 +0700

    ModelMaterial

--- shimas/src/App.tsx
+++ shimas/src/App.tsx
@@ -2,16 +2,18 @@ import BlenderToThree from "@comp/BlenderToThree"
 import SyslarSysterm from "@comp/SyslarSysterm"
 import Cannon from '@comp/Cannon'
 import EvenSquaresInThree from "@comp/EvenSquaresInThree"
+import ModelMaterial from "@comp/ModelMaterial"
 
 function App() {
- 
+
 
   return (
     <>
-     {/*       <SyslarSysterm/>
-         <BlenderToThree/> */}
-   {/*   <Cannon/> */}
-    <EvenSquaresInThree/>  
+      {/*  <SyslarSysterm/>*/}
+      {/*  <BlenderToThree/>*/}
+      {/*  <Cannon/>*/}
+       <EvenSquaresInThree/>
+    {/*   <ModelMaterial/> */}
     </>
   )
 }
---
+++ shimas/src/components/ModelMaterial.tsx
@@ -0,0 +1,102 @@
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import * as dat from 'dat.gui'
+
+const ModelMaterial = () => {
+
+    const fileUrl = new URL('../assets/materialOfLoaderModel/Donkey.gltf', import.meta.url)
+    const renderer = new THREE.WebGLRenderer({ antialias: true })
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    document.body.appendChild(renderer.domElement);
+
+    const scene = new THREE.Scene();
+
+    const camera = new THREE.PerspectiveCamera(
+        50,
+        window.innerWidth / window.innerHeight,
+        0.1,
+        2000
+    );
+    renderer.setClearColor(0xb29fc8);
+    const orbit = new OrbitControls(camera, renderer.domElement);
+    camera.position.set(10, 10, 10);
+    orbit.update();
+
+    const grid = new THREE.GridHelper(30, 30)
+    scene.add(grid)
+    const ambientLight = new THREE.AmbientLight(0xededed, 0.8);
+    scene.add(ambientLight);
+
+
+    const directionalLight = new THREE.DirectionalLight(0xFFFFFF, 1);
+    scene.add(directionalLight);
+    directionalLight.position.set(10, 11, 7);
+
+    const gui = new dat.GUI();
+
+    const assetLoader = new GLTFLoader();
+    const options = {
+        'Main': 0x2F3130,
+        'Main light': 0x7C7C7C,
+        'Main dark': 0x0A0A0A,
+        'Hooves': 0x0F0B0D,
+        'Hair': 0x0A0A0A,
+        'Muzzle': 0x0B0804,
+        'Eye dark': 0x020202,
+        'Eye white': 0xBEBEBE
+    }
+
+    assetLoader.load(fileUrl.href, function (gltf) {
+        const model = gltf.scene
+        scene.add(model)
+        gui.addColor(options, 'Main').onChange(function(e) {
+            model.getObjectByName('Cube').material.color.setHex(e);
+        });
+        gui.addColor(options, 'Main light').onChange(function(e) {
+            model.getObjectByName('Cube_1').material.color.setHex(e);
+        });
+        gui.addColor(options, 'Main dark').onChange(function(e) {
+            model.getObjectByName('Cube_2').material.color.setHex(e);
+        });
+        gui.addColor(options, 'Hooves').onChange(function(e) {
+            model.getObjectByName('Cube_3').material.color.setHex(e);
+        });
+        gui.addColor(options, 'Hair').onChange(function(e) {
+            model.getObjectByName('Cube_4').material.color.setHex(e);
+        });
+        gui.addColor(options, 'Muzzle').onChange(function(e) {
+            model.getObjectByName('Cube_5').material.color.setHex(e);
+        });
+        gui.addColor(options, 'Eye dark').onChange(function(e) {
+            model.getObjectByName('Cube_6').material.color.setHex(e);
+        });
+        gui.addColor(options, 'Eye white').onChange(function(e) {
+            model.getObjectByName('Cube_7').material.color.setHex(e);
+        });
+    }, undefined, function (error) {
+        console.error(error);
+    }
+    )
+
+
+    function animate() {
+        renderer.render(scene, camera);
+    }
+
+    renderer.setAnimationLoop(animate);
+
+    window.addEventListener('resize', function () {
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+        renderer.setSize(window.innerWidth, window.innerHeight);
+    });
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default ModelMaterial
